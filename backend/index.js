8564c2df8d3556d2410e4473b1dbbf8bf017670e
Your Name <nagtanmay01@gmail.com>
2025-10-28 22:44:50 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
+import userRoutes from './routes/user.route.js';
+import companyRoutes from './routes/company.route.js';
 dotenv.config({});
 const app = express();
 // Middleware to parse JSON bodies
@@ -15,6 +17,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/company', companyRoutes);
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
